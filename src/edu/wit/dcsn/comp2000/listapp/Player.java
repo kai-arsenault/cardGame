4f2d7a6b873507221560c4de02c35676d3a5b849
Ernest Shedden <48100899+sheddene@users.noreply.github.com>
2019-04-19 13:42:16 -0400

    Added comments

--- src/edu/wit/dcsn/comp2000/listapp/Player.java
+++ src/edu/wit/dcsn/comp2000/listapp/Player.java
@@ -21,67 +21,94 @@ package edu.wit.dcsn.comp2000.listapp;
 
 
 /**
- * @author 
- * @version
+ * @author Ernest Shedden
+ * @version 1.0
  *
  */
-public class Player
-	{
-	private Hand hand ;
+public class Player {
+	
+	private Hand hand;
 	private Card playedCard = null;
 	private static int nextId = 1;
 	private final int id;
 
 	/**
-	 * 
+	 * Default constructor
 	 */
 	public Player() {
 		this.hand = new Hand();
 		id = Player.nextId++;
 	}	// end constructor
-
+	
+	/**
+	 * Retrieve player's ID
+	 * @return the id of the player
+	 */
 	public int getID() {
 		return id;
 	}
-
+	
+	/**
+	 * Add a card to bottom of player's hand
+	 * @param newCard card to be added
+	 */
 	public void addCard(Card newCard) {
 		hand.add(newCard);
 	}
-
+	
+	/**
+	 * Remove a card from top of player's hand
+	 * @return card that was removed
+	 */
 	public Card removeCard() {
 		return hand.remove();
 	}
 
+	/**
+	 * Checks if player's hand is empty
+	 * @return true if hand is empty
+	 */
 	public boolean isEmpty() {
 		return hand.isEmpty();
 	}
-
+	
+	/**
+	 * Removes card from player's hand, sets it as the players playedCard
+	 */
 	public void playCard() {
 		playedCard = hand.remove();
 	}
 
+	/**
+	 * Gets the player's played card
+	 * @return card that was played
+	 */
 	public Card getPlayedCard() {
 		return playedCard;
 	}
 
+	/**
+	 * Remove player's played card
+	 * @return card that was removed
+	 */
 	public Card removePlayedCard() {
 	    Card output = playedCard;
 		playedCard = null;
 		return output;
 	}
 	
+	/**
+	 * Counts the number of cards in a player's hand
+	 * @return number of cards
+	 */
 	public int getCardCount() {
 		return hand.getSize();
 	}
 
-
 	/**
 	 * @param args
 	 */
-	public static void main( String[] args )
-		{
-		// TODO Auto-generated method stub
-
-		}	// end main()
-
-	}	// end class Player
+	public static void main( String[] args ) {
+		
+	}	// end main()
+}	// end class Player
